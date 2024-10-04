07f6f6bcbd1150136890abb18a53087c227f5c81
AmNetDT <amnetdt@yahoo.com>
2024-10-04 10:02:03 +0100

    Initiating the Git Repository

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/obadosunmu/Documents/newcommerce/chooselifeandpeace/app/layout\":[\"static/media/eafabf029ad39a43-s.p.woff2\",\"static/media/0484562807a97172-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
