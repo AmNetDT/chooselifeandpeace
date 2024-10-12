a1a940088544f5ec93771b679dc34a9ac111e3af
AmNetDT <amnetdt@yahoo.com>
2024-10-12 13:19:48 +0100

    Select Payment Method

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,6 +3,12 @@ export const APP_NAME =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Fashion & Clothing'
 
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(', ')
+  : ['PayPal', 'Stripe']
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || 'PayPal'
+
 export const signInDefaultValues = {
   email: '',
   password: '',
